579d7bc194b2e00ee74bb4f3c93e6a82eee76d67
mmasomi <mmasomi73@gmail.com>
2021-06-20 22:52:00 +0430

    🤙 v 0.0.3
    predictor - 02
    image segmentation

--- predictor_2.py
+++ predictor_2.py
@@ -3,19 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def accessPiexl(img):
+def accessPixel(img):
     height = img.shape[0]
     width = img.shape[1]
     for i in range(height):
-       for j in range(width):
-           img[i][j] = 255 - img[i][j]
+        for j in range(width):
+            img[i][j] = 255 - img[i][j]
     return img
 
-def accessBinary(img, threshold=128):
-    img = accessPiexl(img)
-    kernel = np.ones((3, 3), np.uint8)
-    img = cv2.dilate(img, kernel, iterations=1)
-    _, img = cv2.threshold(img, threshold, 0, cv2.THRESH_TOZERO)
+
+def accessBinary(img, threshold=10):
+    img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+    thresh, img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+    img = accessPixel(img)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+    img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
     return img
 
 
@@ -39,6 +41,7 @@ def extractPeek(array_vals, min_vals=10, min_rect=20):
             extrackPoints.remove(point)
     return extrackPoints
 
+
 def findBorderHistogram(path):
     borders = []
     img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
@@ -54,34 +57,46 @@ def findBorderHistogram(path):
             borders.append(border)
     return borders
 
-def findBorderContours(path, maxArea=50):
-    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+def findBorderContours(path, maxArea=1):
+    img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    threshold = 100
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = accessBinary(img)
-    contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    img = cv2.Canny(img, threshold, threshold * 2)
+
+    contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     borders = []
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
-        if w*h > maxArea:
-            border = [(x, y), (x+w, y+h)]
+        if w * h > maxArea:
+            border = [(x, y), (x + w, y + h)]
             borders.append(border)
     return borders
 
 
 def showResults(path, borders, results=None):
-    img = cv2.imread(path)
+    img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    img = accessBinary(img)
     print(img.shape)
     for i, border in enumerate(borders):
         cv2.rectangle(img, border[0], border[1], (0, 0, 255))
         if results:
             cv2.putText(img, str(results[i]), border[0], cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 1)
-        #cv2.circle(img, border[0], 1, (0, 255, 0), 0)
-    cv2.imshow('test', img)
-    cv2.waitKey(0)
+        # cv2.circle(img, border[0], 1, (0, 255, 0), 0)
+    plt.imshow(img)
+    plt.imsave('saved_figure.png', img)
+
 
 def transMNIST(path, borders, size=(28, 28)):
-    imgData = np.zeros((len(borders), size[0], size[0], 1), dtype='uint8')
-    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    imgData = np.zeros((len(borders), size[0], size[0], 3, 1), dtype='uint8')
+
+    img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    threshold = 100
+    img = cv2.Canny(img, threshold, threshold * 2)
     img = accessBinary(img)
+
     for i, border in enumerate(borders):
         borderImg = img[border[0][1]:border[1][1], border[0][0]:border[1][0]]
         extendPiexl = (max(borderImg.shape) - min(borderImg.shape)) // 2
@@ -91,23 +106,19 @@ def transMNIST(path, borders, size=(28, 28)):
         imgData[i] = targetImg
     return imgData
 
+
 def predict(modelpath, imgData):
-    from keras import models
-    my_mnist_model = models.load_model(modelpath)
-    print(my_mnist_model.summary())
-    img = imgData.astype('float32') / 255
-    results = my_mnist_model.predict(img)
     result_number = []
-    for result in results:
-        result_number.append(np.argmax(result))
     return result_number
 
+
 if __name__ == '__main__':
-    # path = 'test1.png'
-    path = 'captcha/1624173482639230700.png'
+    path = 'captcha/1624173627163216600.png'
     model = 'model.h5'
     borders = findBorderContours(path)
     imgData = transMNIST(path, borders)
-    results = predict(model, imgData)
-    showResults(path, borders, results)
-
+    # results = predict(model, imgData)
+    showResults(path, borders, range(len(borders)))
+    # plt.imshow(borders)
+    # plt.imsave('saved_figure.png', borders)
+    print(borders)
